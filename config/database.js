6d738400502c64f825e2381feb8cc045511c6526
yuanchaoli <54liyuanchao@gmail.com>
2018-06-07 16:29:03 -0500

    linked

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://Yuanchao Li:0ooo00ooo0@ds151970.mlab.com:51970/meanauthapp',
+  database: 'mongodb://li:0ooo00ooo0@ds151970.mlab.com:51970/meanauthapp',
   secret: '0ooo00ooo0'
 }
