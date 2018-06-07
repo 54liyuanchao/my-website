dc941122ea0875f65a235e4e55e4a99e31335686
yuanchaoli <54liyuanchao@gmail.com>
2018-06-07 15:56:24 -0500

    linkeddb

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://Yuanchao Li:Housegoat@64@ds151970.mlab.com:51970/meanauthapp',
   secret: '0ooo00ooo0'
 }
